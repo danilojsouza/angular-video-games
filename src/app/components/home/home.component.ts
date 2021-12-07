444bc2b525a60277b4e5aa70d077b58bea150e5c
Danilo Souza <danilosouza.bm@gmail.com>
2021-12-07 19:02:43 -0300

    Implement and Style HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  public sort: string;
 
-  ngOnInit(): void {
+  constructor() {
+    this.sort = "Avaliados";
   }
 
 }
