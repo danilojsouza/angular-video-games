392176c7b482a538edb960b1ae6c4e04a439733f
Danilo Souza <danilosouza.bm@gmail.com>
2021-12-07 20:34:01 -0300

    Implement HomeComponent with API

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpService } from './../../services/http.service';
+import { Game, APIResponse } from './../../api.models';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  public sort: string;
+  public ordem: string = "";
+  public jogos: Array<Game> = [];
 
-  constructor() {
-    this.sort = "Avaliados";
+  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if(params['game-search'])
+        this.searchGames('metacrit', params['game-search']);
+      else
+        this.searchGames('metacrit');
+    });
+  }
+  searchGames(ordenacao: string, pesquisa?: any) {
+    this.httpService.findAll(ordenacao, pesquisa).subscribe((listaJogos: APIResponse<Game>) => {
+      this.jogos = listaJogos.results;
+      console.log(this.jogos);
+    });
   }
 
 }
